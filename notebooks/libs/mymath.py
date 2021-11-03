88579deb7485a858ce1fbed5c327623d3127863a
Jonathan Randolph <Jrandolph011@gmail.com>
2021-11-02 20:12:18 -0500

    adding multiplier and divider functions

--- notebooks/libs/mymath.py
+++ notebooks/libs/mymath.py
@@ -3,3 +3,15 @@ def adder(a, b):
         Function adds values from args a and b^2
     """
     return a+(b*b)
+
+def multiplier(a, b):
+    """
+        Function multiplies values from args a and b^2
+    """
+    return a*(b*b)
+
+def divider(a, b):
+    """
+        Function divides values from args a and b^2
+    """
+    return a/(b*b)
