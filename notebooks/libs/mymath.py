2d6f8c3c09cacbf2da4794102b33ca92f0c70426
Jonathan Randolph <Jrandolph011@gmail.com>
2021-11-02 20:13:35 -0500

    adding typehinting to mymath.py

--- notebooks/libs/mymath.py
+++ notebooks/libs/mymath.py
@@ -1,16 +1,18 @@
-def adder(a, b):
+from typing import Union
+
+def adder(a: Union[int, float], b: Union[int, float]):
     """
         Function adds values from args a and b^2
     """
     return a+(b*b)
 
-def multiplier(a, b):
+def multiplier(a: Union[int, float], b: Union[int, float]):
     """
         Function multiplies values from args a and b^2
     """
     return a*(b*b)
 
-def divider(a, b):
+def divider(a: Union[int, float], b: Union[int, float]):
     """
         Function divides values from args a and b^2
     """
